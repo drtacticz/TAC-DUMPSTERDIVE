9be57de40dd3c758f3eeac685c67b681c56727cc
T A C <drtacticzz@gmail.com>
2024-02-03 17:43:13 +0000

    Add files via upload

---
+++ TAC-DUMPSTERDIVE/config.lua
@@ -0,0 +1,35 @@
+Config = {
+    isTarget = true,
+
+    ghxstyErr = "You already looked in the dumpster.",
+    ghxstyStop = "You stopped looking in the dumpster.",
+    ghxstySearching = "Looking in dumpster....",
+
+    timeLooking = 2, -- jake you can change this depending how long you want it to take or you can math.random( [m][, n] ) it for random time
+
+    addonItemsActive = true, -- if this is true it will activate an additional item given to player
+
+    itemTable = { -- jake add normal items here
+        [1] = {item = 'rubber', amount = math.random(1,5)},
+    },
+
+    addonItemsTable = { -- these are the additional items
+        [1] = {item = 'rubber', amount = math.random(1,5)},
+    },
+
+    areDumps = {
+        'prop_dumpster_01a',
+        'prop_dumpster_02a',
+        'prop_dumpster_02b',
+        'prop_dumpster_3a',
+        'prop_dumpster_4a',
+        'prop_dumpster_4b'
+    },
+
+    icons = {
+        baseIcon = "fas fa-dumpster",
+        lookAt = "fa-solid fa-hand fa-fade",
+    }
+
+
+}
